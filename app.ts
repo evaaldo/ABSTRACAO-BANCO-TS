89124b68f98a554510633366ce48fe9976fd467f
evaldo <evaaldormj@gmail.com>
2024-01-07 11:13:00 -0300

    Update: App.ts

--- app.ts
+++ app.ts
@@ -1,7 +1,5 @@
 import { CompanyAccount } from "./class/CompanyAccount";
 import { PeopleAccount } from "./class/PeopleAccount";
 
-const evaldo = new PeopleAccount(1, 'Evaldo', 10, 1000, true)
+const evaldo = new PeopleAccount(1, 'Pessoa', 10, 1000, true)
 const empresa = new CompanyAccount('Empresa', 11, 1000000, true)
-
-empresa.getLoan(50000)
